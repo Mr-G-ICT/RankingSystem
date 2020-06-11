4782c12d00f4f62a48cec612950ecb3c658a3a62
Mr B Gristwood <bgristwood@outlook.com>
2020-06-11 12:53:39 +0100

    Add project files.

---
+++ RankingSystem/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RankingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pointsscored;
+            
+            Console.WriteLine("Hello World!");
+
+            RankingSystem determineUserRank = new RankingSystem();
+
+            determineUserRank.setupUser();
+            pointsscored = determineUserRank.completeAnActivity(-4);
+
+            Console.WriteLine(pointsscored + "points scored");
+
+            determineUserRank.setCurrentProgress = pointsscored;
+
+            determineUserRank.CheckForLevelUp();
+
+            Console.WriteLine(determineUserRank.getUserRank);
+
+
+
+        }
+
+
+
+    }
+}
---
+++ RankingSystem/RankingSystem.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RankingSystem
+{
+    class RankingSystem
+    {
+        private struct UserDetails
+        {
+            public int UserRank { get; set; }
+            public int currentProgress;
+
+        }
+
+        private UserDetails currentUser;
+
+        public int getUserRank
+        {
+            get { return currentUser.UserRank; }
+        }
+
+  
+
+        public int setCurrentProgress
+        {
+            set { currentUser.currentProgress = currentUser.currentProgress + value; }
+        }
+        public void setupUser()
+        {
+            //have created this for futureproofing, incase i ever need to get data from a file etc.
+            currentUser.UserRank = -8;
+            currentUser.currentProgress = 0;
+        }
+
+
+        public int completeAnActivity(int ChallengeCompleted)
+        {
+            int diff =  ChallengeCompleted - currentUser.UserRank;
+            int pointsScored = 0;
+
+            if (diff == 0)
+            {
+                pointsScored = 3;
+            }else if(diff == -1)
+            {
+                pointsScored = 1;
+            }else if(diff > 0)
+            {
+                pointsScored = 10 * (diff * diff);
+            }
+
+            return pointsScored;   
+        }
+
+
+        public void updateUserRank()
+        {
+            currentUser.UserRank = currentUser.UserRank + 1;
+
+            if (currentUser.UserRank == 0)
+            {
+                currentUser.UserRank = 1;
+            }
+        }
+
+        public void CheckForLevelUp()
+        {
+
+            if (currentUser.currentProgress >= 100)
+            {
+                updateUserRank();
+                currentUser.currentProgress = currentUser.currentProgress - 100;
+            }
+
+
+        }
+    
+}
+}
